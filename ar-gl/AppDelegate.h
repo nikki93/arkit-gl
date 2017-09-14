07d4aac9edae2a5ae4099f7472c1475dbcd14a6d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2017-09-13 17:29:08 -0700

    Initial Commit

---
+++ ar-gl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ar-gl
+//
+//  Created by Nikhilesh Sigatapu on 9/13/17.
+//  Copyright © 2017 Nikhilesh Sigatapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ar-gl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ar-gl
+//
+//  Created by Nikhilesh Sigatapu on 9/13/17.
+//  Copyright © 2017 Nikhilesh Sigatapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
